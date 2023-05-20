e7ac7b4484a1c3bf0d4e2f851bdbc607e6189256
Columbiysky <c.7843543@gmail.com>
2023-05-20 09:51:03 +0000

    dependenciess installer for docker dev container

---
+++ dev-container-dependencies-installer.sh
@@ -0,0 +1,19 @@
+echo "apt update and upgrade"
+apt update -y
+apt upgrade -y
+
+echo "install nodeJs 19 and dependencies for client app"
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && sudo apt-get install -y nodejs
+cd ./usefulApps/frontend
+npm i
+cd ../..
+
+echo "install python"
+apt install python3 -y
+
+ehco "install golang"
+curl -O https://dl.google.com/go/go1.20.4.linux-amd64.tar.gz
+tar -C /usr/local -xvf go1.20.4.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+rm go1.20.4.linux-amd64.tar.gz
