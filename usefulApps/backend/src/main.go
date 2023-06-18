9899d2ffedcb310a8dd8902acbf2b0205b8e57ed
Columbiysky <c.7843543@gmail.com>
2023-06-18 15:11:40 +0000

    cors fix

--- usefulApps/backend/src/main.go
+++ usefulApps/backend/src/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"main/controllers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST"},
+		AllowHeaders:    []string{"Origin"},
+	}))
+
 	router.GET("/hello/", controllers.Hello)
 	router.POST("/hello/", controllers.HelloObject)
 	router.Run()
