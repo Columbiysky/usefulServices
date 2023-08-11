17f2173dd762cbb52eee16228267305a3e46d306
Columbiysky <c.7843543@gmail.com>
2023-08-11 12:04:14 +0000

    update script for python installation. bumb golang version

--- dev-container-dependencies-installer.sh
+++ dev-container-dependencies-installer.sh
@@ -1,19 +1,41 @@
 echo "apt update and upgrade"
 apt update -y
 apt upgrade -y
+echo "******************************"
 
 echo "install nodeJs 19 and dependencies for client app"
 curl -fsSL https://deb.nodesource.com/setup_19.x | -E bash - && sudo apt-get install -y nodejs
 cd ./usefulApps/frontend
 npm i
 cd ../..
+echo "******************************"
 
 echo "install python"
-apt install python3 -y
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev pkg-config -y
+wget https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tar.xz
+tar -xvf Python-3.11.4.tar.xz
+cd Python-3.11.4/
+./configure --enable-optimizations
+make
+make altinstall
+python3.11 --version
+cd ..
+echo "******************************"
+echo "install pip"
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11 
+pip3.11 -V
+cd ./usefulApps/executors/python
+pip3.11 install -r requirements.txt
+echo "******************************"
+
+rm Python-3.11.4.tar.xz
+rm -rf Python-3.11.4
+echo "******************************"
 
 ehco "install golang"
-curl -O https://dl.google.com/go/go1.20.6.linux-amd64.tar.gz
-tar -C /usr/local -xvf go1.20.6.linux-amd64.tar.gz
+curl -O https://dl.google.com/go/go1.21.0.linux-amd64.tar.gz
+tar -C /usr/local -xvf go1.21.0.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
-rm go1.20.6.linux-amd64.tar.gz
+rm go1.21.0.linux-amd64.tar.gz
+echo "******************************"
