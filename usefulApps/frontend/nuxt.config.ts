3abc48fa073bae81d9b68d771fa52c089db703f3
Columbiysky <c.7843543@gmail.com>
2023-11-14 23:32:08 +0300

    chore(frontend): using nuxt verion of primeVue

--- usefulApps/frontend/nuxt.config.ts
+++ usefulApps/frontend/nuxt.config.ts
@@ -8,5 +8,32 @@ export default defineNuxtConfig({
     build: {
         transpile: ['primevue']
     },
+    modules: [
+        'nuxt-primevue'
+    ],
+    primevue: {
+        usePrimeVue: true,
+        options: {},
+        importPT: undefined,
+        cssLayerOrder: 'tailwind-base, primevue, tailwind-utilities',
+        components: {
+            prefix: '',
+            name: undefined,
+            include: undefined,
+            exclude: undefined
+        },
+        directives: {
+            prefix: '',
+            name: undefined,
+            include: undefined,
+            exclude: undefined
+        },
+        composables: {
+            prefix: '',
+            name: undefined,
+            include: undefined,
+            exclude: undefined
+        }
+    },
     ssr: false,
 })
