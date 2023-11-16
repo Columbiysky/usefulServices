bb6c2db7194cc63452bfb698f348dcec53c966b6
Columbiysky <c.7843543@gmail.com>
2023-11-17 01:51:46 +0300

    feat(sso): added TableName() for every model

--- usefulApps/sso/models/models.go
+++ usefulApps/sso/models/models.go
@@ -6,13 +6,25 @@ type Account struct {
 	Password string `gorm:"not null" json:"account_password"`
 }
 
+func (Account) TableName() string {
+	return "sso.accounts"
+}
+
 type Token struct {
 	Id         int64  `gorm:"not null" json:"token_id"`
 	TokenValue string `gorm:"not null" json:"token_value"`
+}
+
+func (Token) TableName() string {
+	return "sso.tokens"
 }
 
 type AccountToken struct {
 	Id             int64 `gorm:"not null" json:"account_token_id"`
 	AccountReferId int64
 	TokenReferId   int64
+}
+
+func (AccountToken) TableName() string {
+	return "sso.accounts_tokens"
 }
