505052adbf5bf14faa32925b69491a3a52ff16dd
Columbiysky <c.7843543@gmail.com>
2024-01-03 23:32:07 +0300

    feat(backend): added account login and password mandatory

--- usefulApps/backend/src/logic/account/accountLogic.go
+++ usefulApps/backend/src/logic/account/accountLogic.go
@@ -3,6 +3,7 @@ package logic
 import (
 	accDb "backend/dbLogic/account"
 	"backend/models"
+	"log"
 )
 
 func GetAccountById(id int64) *models.Account {
@@ -11,6 +12,11 @@ func GetAccountById(id int64) *models.Account {
 }
 
 func RegisterAccount(account models.Account) int {
+	if account.Login == "" || account.Password == "" {
+		log.Println("Account login and password must be not empty")
+		return -1
+	}
+
 	newId := accDb.RegisterAccount(account)
 	return newId
 }
