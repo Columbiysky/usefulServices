a6a75ba76b06d7438c88785e0b6378f8ed478fd4
Columbiysky <c.7843543@gmail.com>
2024-01-04 21:51:03 +0300

    feat(backend): added neccessary models

--- usefulApps/backend/src/models/models.go
+++ usefulApps/backend/src/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type Account struct {
 	Id       int    `gorm:"not null" json:"account_id"`
 	Login    string `gorm:"not null" json:"account_login"`
@@ -8,4 +10,24 @@ type Account struct {
 
 func (Account) TableName() string {
 	return "sso.accounts"
+}
+
+type Token struct {
+	Id               int       `gorm:"not null" json:"token_id"`
+	TokenValue       string    `gorm:"not null" json:"token_value"`
+	LastActivityTime time.Time `gorm:"not null" json:"last_activity_time"`
+}
+
+func (Token) TableName() string {
+	return "sso.tokens"
+}
+
+type AccountToken struct {
+	Id        int `gorm:"not null" json:"account_token_id"`
+	AccountId int `gorm:"not null" json:"account_id"`
+	TokenId   int `gorm:"not null" json:"token_id"`
+}
+
+func (AccountToken) TableName() string {
+	return "sso.accounts_tokens"
 }
