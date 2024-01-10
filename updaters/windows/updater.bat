eab76900232f6847b5ae4c87e21b554f13745c82
Columbiysky <c.7843543@gmail.com>
2024-01-10 21:10:53 +0300

    feat(updaters): added windows updater

---
+++ updaters/windows/updater.bat
@@ -0,0 +1,19 @@
+cd ..\..\usefulApps
+cd backend\src
+go get .
+
+cd ..\..
+cd executors\python
+pip install -r .\requirements.txt -U
+
+cd ..\..
+cd frontend
+npm i
+
+cd ..\..
+cd sso
+go get .
+
+cd ..\..
+cd tools\dbTool
+go get .
