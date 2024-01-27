00fba9a8c76c241ae7290fcada23260369fe4859
Columbiysky <c.7843543@gmail.com>
2024-01-28 00:54:36 +0300

    fix(frontend): nuxt asked for the fix

--- usefulApps/frontend/logic/webhooks/httpWrappers/httpWrapper.ts
+++ usefulApps/frontend/logic/webhooks/httpWrappers/httpWrapper.ts
@@ -1,12 +1,11 @@
 import _ from "lodash";
-import { UseFetchOptions } from "nuxt/app";
+import type { UseFetchOptions } from "nuxt/app";
 
 export default class HttpWrapper {
     HttpGet(url: string, options?: UseFetchOptions<any>) {
         if (_.isNil(options)) {
             options = <UseFetchOptions<any>>{
                 method: "GET",
-
             };
         } else {
             options.method = "GET";
