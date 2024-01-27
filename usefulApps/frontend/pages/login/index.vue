b52c9f60d3860850d718afc34d2b82e9d9a165bb
Columbiysky <c.7843543@gmail.com>
2024-01-28 01:08:13 +0300

    feat(frontend): login page styles changed. login function realized

--- usefulApps/frontend/pages/login/index.vue
+++ usefulApps/frontend/pages/login/index.vue
@@ -9,23 +9,54 @@
                 <Password id="password" v-model="password" :feedback="false" />
             </div>
 
-            <Button class="button-input" label="Login" @click="buttonClick()"></Button>
+            <Button class="button-input" label="Login" @click="loginFunc()"></Button>
+        </div>
+
+        <div class="error-box">
+            <span> {{ errorMessage }}</span>
         </div>
     </div>
 </template>
 <script lang="ts">
+import type { IAccountLoginDto } from '~/data/account/IAccountLoginDto';
+import type { ITokenValueDto } from '~/data/account/ITokenValueDto';
+import type { IErrorMessage } from '~/data/general/IErrorMessage';
+import HttpWrapper from '~/logic/webhooks/httpWrappers/httpWrapper';
+
 export default {
     data() {
         return {
             login: '',
             password: '',
-            error: '',
+            errorMessage: '',
         }
     },
     methods: {
-        buttonClick() {
-            console.log(this.login)
-            console.log(this.password)
+        loginFunc() {
+            const _this = this;
+            const wrapper = new HttpWrapper();
+            wrapper.HttpPost('http://localhost:8081/login/', {
+                onRequest({ request, options }) {
+                    options.body = <IAccountLoginDto>{
+                        account_login: _this.login,
+                        account_password: _this.password
+                    }
+                },
+                onResponse({ request, response, options }) {
+                    if (response.status === 200) {
+                        const res = response._data as ITokenValueDto;
+                        console.log(res);
+                    }
+                },
+                onRequestError({ request, options, error }) {
+                    console.log(error);
+                },
+                onResponseError({ request, response, options }) {
+                    const err = response._data as IErrorMessage;
+                    _this.errorMessage = err.message
+                    console.log(err.message);
+                }
+            });
         }
     }
 }
@@ -35,8 +66,8 @@ export default {
 .container {
     display: block;
     padding-top: 20%;
-    padding-bottom: 15%;
-    background-color: rgb(236, 241, 246);
+    padding-bottom: 11%;
+    background-color: rgb(101, 110, 120);
 }
 
 .login-box {
@@ -52,5 +83,11 @@ export default {
 
 .button-input {
     margin-top: 20px;
+}
+
+.error-box {
+    color: red;
+    padding-top: 50px;
+    text-align: center;
 }
 </style>
