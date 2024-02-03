49fdb0384ebf3245311ebc4ac645c08fc9e5c134
Columbiysky <c.7843543@gmail.com>
2024-02-03 22:44:23 +0300

    feat(frontend):restyling

---
+++ usefulApps/frontend/pages/main/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <p>hsdfaksdjfhalksdhj</p>
+    </div>
+</template>
+<style>
+.container {
+    display: block;
+    height: max-content;
+    padding-top: 20%;
+    padding-bottom: 11%;
+    background-color: rgb(16, 45, 78);
+}
+
+p {
+    color: white;
+}
+</style>
