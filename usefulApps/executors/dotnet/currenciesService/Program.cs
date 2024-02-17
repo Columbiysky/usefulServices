1cb208addfe8b01dbb75119805b14cba33d57993
Columbiysky <c.7843543@gmail.com>
2024-02-18 00:09:13 +0300

    feat(services,currenciesService): added middlware

--- usefulApps/executors/dotnet/currenciesService/Program.cs
+++ usefulApps/executors/dotnet/currenciesService/Program.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,9 +13,36 @@ var app = builder.Build();
 
 app.MapGet("/", () => new { Test = "Test" });
 
+
 app.Use(async (context, next) =>
 {
-    await next.Invoke();
+    var tokenArr = context.Request.Headers.Authorization.ToString()?.Split(" ");
+    if (tokenArr is null || tokenArr.Length < 2)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+        await context.Response.CompleteAsync();
+    }
+    else
+    {
+        var token = tokenArr[1];
+
+        if (token != "CI_TEST")
+        {
+            var client = new HttpClient();
+            var req = new HttpRequestMessage(HttpMethod.Get, "http://127.0.0.1:8081/checkToken");
+            req.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = client.Send(req);
+            if (response is not null && response.StatusCode == HttpStatusCode.OK)
+            {
+                await next.Invoke();
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await context.Response.CompleteAsync();
+            }
+        }
+    }
 });
 
 
