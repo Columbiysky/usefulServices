8b7b4325012dc3dd24f9188367f6898fa0bbd355
Columbiysky <c.7843543@gmail.com>
2024-05-05 22:28:24 +0300

    finance(chainlink): ezETH/ETH contract wrapper added

--- usefulApps/executors/ts/main.ts
+++ usefulApps/executors/ts/main.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
+import EzEthToEthArbMainWrapper from "./wrappedContracts/arbMain/ezEthToEthWrapper";
 import WstEthToEthArbMainWrapper from "./wrappedContracts/arbMain/wstEthToEthWrapper";
 
 const arbMainRpcLink = JSON.parse(fs.readFileSync('./configs/arbMainRpcLink.json', 'utf8'));
 const wstEthToEthArbMainWrapper = new WstEthToEthArbMainWrapper(arbMainRpcLink.provider);
-wstEthToEthArbMainWrapper.get().then((res) => console.log(res));
+const ezEthToEthArbMainWrapper = new EzEthToEthArbMainWrapper(arbMainRpcLink.provider);
+wstEthToEthArbMainWrapper.get().then((res) => console.log("wstETH\\ETH = " + res));
+ezEthToEthArbMainWrapper.get().then((res) => console.log("ezETH\\ETH = " + res));
