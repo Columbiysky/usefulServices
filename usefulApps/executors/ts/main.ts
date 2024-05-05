025b36092ffaf5d6af2774db5dad14b206382973
Columbiysky <c.7843543@gmail.com>
2024-05-05 22:51:33 +0300

    finance(chainlink): placed method getting data on start and into interval (once in hour)

--- usefulApps/executors/ts/main.ts
+++ usefulApps/executors/ts/main.ts
@@ -5,5 +5,14 @@ import WstEthToEthArbMainWrapper from "./wrappedContracts/arbMain/wstEthToEthWra
 const arbMainRpcLink = JSON.parse(fs.readFileSync('./configs/arbMainRpcLink.json', 'utf8'));
 const wstEthToEthArbMainWrapper = new WstEthToEthArbMainWrapper(arbMainRpcLink.provider);
 const ezEthToEthArbMainWrapper = new EzEthToEthArbMainWrapper(arbMainRpcLink.provider);
-wstEthToEthArbMainWrapper.get().then((res) => console.log("wstETH\\ETH = " + res));
-ezEthToEthArbMainWrapper.get().then((res) => console.log("ezETH\\ETH = " + res));
+
+getDataFromContracts();
+setInterval(
+    getDataFromContracts,
+    1000 * 60 * 60 // 1 hour in milliseconds
+)
+
+function getDataFromContracts() {
+    wstEthToEthArbMainWrapper.get().then((res) => console.log("wstETH\\ETH = " + res));
+    ezEthToEthArbMainWrapper.get().then((res) => console.log("ezETH\\ETH = " + res));
+}
