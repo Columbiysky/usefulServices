ac58cbc4fd19714221cf2cb2133661b1134dd860
Columbiysky <c.7843543@gmail.com>
2024-06-02 23:47:40 +0300

    feat!(chainlink): removed background module project cuz dont need it anymore

--- usefulApps/services/ts/webApiForChainlink/backgruondModule/wrappedContracts/baseWrapper.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-
-export interface IBaseWrapper {
-    pairName: string;
-    provider: ethers.JsonRpcProvider,
-    contractAddress: string,
-    get(): Promise<number>;
-}
