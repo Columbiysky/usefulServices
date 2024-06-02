f56e052769df0a4b26719bc7684d5f436d311bc6
Columbiysky <c.7843543@gmail.com>
2024-06-02 22:11:10 +0300

    db(migration): added nullable column 'LastTimeUpdate' to 'Pair' table

--- usefulApps/tools/DbCreatorAndMigrator/Migrations/20240602191014_PairAddLastTimeUpdateNullable.Designer.cs
+++ usefulApps/tools/DbCreatorAndMigrator/Migrations/20240602191014_PairAddLastTimeUpdateNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DbCreatorAndMigrator;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DbCreatorAndMigrator.Migrations
 {
     [DbContext(typeof(UsefulServicesContext))]
-    partial class UsefulServicesContextModelSnapshot : ModelSnapshot
+    [Migration("20240602191014_PairAddLastTimeUpdateNullable")]
+    partial class PairAddLastTimeUpdateNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- usefulApps/tools/DbCreatorAndMigrator/Models/Finance/Pair.cs
+++ usefulApps/tools/DbCreatorAndMigrator/Models/Finance/Pair.cs
@@ -5,5 +5,6 @@ namespace DbCreatorAndMigrator.Models.Finance
         public long Id { get; set; }
         public required string Name { get; set; }
         public double Price { get; set; }
+        public DateTimeOffset? LastTimeUpdate { get; set; }
     }
 }
