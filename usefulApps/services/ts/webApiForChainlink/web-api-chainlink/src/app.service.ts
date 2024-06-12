2e9a72196cdbf9b6b730665adea3937ec79bec02
Columbiysky <c.7843543@gmail.com>
2024-06-12 18:20:59 +0300

    feat:added cron job

--- usefulApps/services/ts/webApiForChainlink/web-api-chainlink/src/app.service.ts
+++ usefulApps/services/ts/webApiForChainlink/web-api-chainlink/src/app.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from "@nestjs/common";
-import { PairDataLogic } from "./innerLogic/pairDataLogic";
+import { CronJob } from 'cron';
 import { PairsEnum } from "./core/pairsEnum";
+import { PairDataLogic } from "./innerLogic/pairDataLogic";
 
 @Injectable()
 export class AppService {
   constructor(private readonly pairDataLogic: PairDataLogic) {
+    const that = this;
+    const job = new CronJob(
+      '0 22 * * * *', // cronTime
+      function () {
+        that.pairDataLogic
+          .getDataFromContracts()
+          .then(() => {
+            console.log("Smart contracts data has been updated from cron"); 
+            console.log(new Date().toISOString()); 
+          });
+      }, // onTick
+      null, // onComplete
+      true, // start
+      'Europe/Moscow' // timeZone
+    );
+
+    job.start();
+    
     this.pairDataLogic
       .getDataFromContracts()
-      .then(() => console.log("Smart contracts data has been updated"));
+      .then(() => console.log("Smart contracts data has been updated from start"));
   }
 
   async getPairData(pairName: string): Promise<number> {
